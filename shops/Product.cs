7a8bd6f00264b572b0ef7318bdbd66a70a407080
Anuushka (Ujin) <35764723+anuushka@users.noreply.github.com>
2020-10-19 18:36:43 +0800

    Add files via upload

---
+++ shops/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Guid;
+
+namespace Shop
+{
+    public class Product
+    {
+        private  string _name;
+        private  string _uuid;
+        public Product(string name)
+        {
+            this._name = name;
+            this._uuid = Guid.NewGuid().ToString();
+        }
+        public string Name => _name;
+        public string Uuid => _uuid;
+    }
+}
